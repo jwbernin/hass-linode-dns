4b2dba8bdc5efa2b90235903ae7af7af88fa701f
John Berninger <john.berninger@gmail.com>
2021-06-08 17:09:06 -0400

    get domain ID

--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/with-contenv bashio
 
-echo Hello world!
-echo -n "My IP is: "
-curl http://ipinfo.io/ip
+PUBLICIP=`curl -s http://ipinfo.io/ip`
+
+LINODE_TOKEN=$(bashio::config 'apiToken')
+DOMAIN=$(bashio::config 'domain')
+
+# Get the domain ID of the domain we're interested in
+DOMAINID=`curl -s -H \"Authorization: Bearer ${LINODE_TOKEN}\" https://api.linode.com/v4/domains | jq '.data[] | select(.domain == \"$(DOMAIN)\") .id'`
+
+echo -n DOMAINID:
+echo ${DOMAINID}
+
+#curl -H "Content-Type: application/json" \
+#     -H "Authorization: Bearer $LINODE_TOKEN" \
+#     -X POST -d '{
+#       "type": "A",
+#       "name": "$(bashio::config 'hostname')",
+#       "target": "$PUBLICIP",
+#       "priority": 50,
+#       "ttl_sec": 604800
+#     }' \
+#     https://apilinode.com/v4/domains/${DOMAINID}/records
+
